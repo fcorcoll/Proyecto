6f26e429f068a737986fcf3cb74eaf8aee131949
fcorcoll <francesc901@gmail.com>
2022-01-14 14:10:41 +0100

    last

--- main.js
+++ main.js
@@ -18,32 +18,33 @@ let precio = prompt("Ingresa el presupuesto estimado:");
             console.log("Presupuesto estimado del cliente es: " + precio);
             return precio;
 }
-
+   
 let resultado = solicitarPresupuesto();
+
 
     if (resultado < 300){
         console.log("Configuración no disponible");
     }
-    else if (resultado < 500 && resultado > 300){
+    else if (resultado <= 500 && resultado > 300){
         const configuracion1 = new Configuracion("Tipo Tier5","Procesador Intel Core i5-10400/i3, 8gbRam DDr4,Gráfica HDintel Integrada");
         console.log(configuracion1.tipo);
         console.log(configuracion1.equipacion);
         mostrarDatosPc(configuracion1.tipo,configuracion1.equipacion);
     }
-    else if (resultado < 800 && resultado > 500){
+    else if (resultado <= 800 && resultado > 500){
         const configuracion2 = new Configuracion("Tipo Tier4","Procesador Intel Core i5-10400, 16gbRam,Gráfica GTX1650/Gtx1050ti");
         console.log(configuracion2.tipo);
         console.log(configuracion2.equipacion);
         mostrarDatosPc(configuracion2.tipo,configuracion2.equipacion);
     }
-    else if (resultado < 1000 && resultado > 800){
+    else if (resultado <= 1000 && resultado > 800){
         
         const configuracion3 = new Configuracion("Tipo Tier3","Procesador AMD Ryzen5/ o Intel core i5, 16gbRam,Gráfica GTX1650");
         console.log(configuracion3.tipo);
         console.log(configuracion3.equipacion);
         mostrarDatosPc(configuracion3.tipo,configuracion3.equipacion);
     }
-    else if (resultado < 1200 && resultado > 1000){
+    else if (resultado <= 1200 && resultado > 1000){
      
         const configuracion4 = new Configuracion("Tier 2","Procesador AMD Ryzen5/ o Intel core i7, 16gbRam,Gráfica 2060ti/2060super");
         console.log(configuracion4.tipo);
@@ -61,7 +62,9 @@ let resultado = solicitarPresupuesto();
 
     else{
         console.warn("no se trata de un dato válido, ingrese otro valor.")
+
     }
+
 
     function mostrarDatosPc(tier,contenido){
 
