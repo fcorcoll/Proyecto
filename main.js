13614c565336898c985650311b63238411af2b70
fcorcoll <francesc901@gmail.com>
2022-01-20 02:45:51 +0100

    arrays

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 
-// Primer test de la entrega de proyecto
+//FRANCESC CORCOLL 
+// Primer  de la entrega de proyecto
 //EN la siguiente entrega posiblemente cree un array con todas las configuraciones de pcs
 //y con los metodos find y demás acceda a cada uno de ellos pero para esta no me da tiempo así que
 //los datos del pc no serán coherentes del todo ya que es temporal.
 
 precio = 0;
+precioProgramas = 0;
+const softwares = [{id :"windows10", valor: 50},
+                   {id :"nvidia", valor: 10},
+                   {id :"office", valor: 100},
+                   {id :"chrome", valor: 0},
+                   {id :"driversPack", valor: 25}]; 
 
 //es el antiguo constructor del objeto para como vimos en clase voy a generar una nueva clase para ello.
 //function Configuracion (tipo,equipacion){
@@ -67,7 +74,53 @@ let resultado = solicitarPresupuesto();
 
     }
 
+    
 
+    let programas = prompt("Desea obtener algún tipo de software con la configuración?:");
+
+    if(programas == "si" || programas == "SI"){
+
+        for (const producto of softwares){
+            alert("software disponible" + " " +producto.id + " " + " con un precio de " + " "+  producto.valor);
+            console.log(producto.id);
+            console.log(producto.valor);
+        }
+        let respuesta = prompt("Que software quieres incluir en la configuración?");
+
+        if (respuesta != ""){
+       // const softwareFiltro = softwares.find((producto) => producto.id == respuesta);
+        //console.log(softwareFiltro);
+        //alert("Se te sumara un precio de" + " "+ softwareFiltro.valor + "$" + " "+ "al total");
+        buscarPrograma(respuesta);
+
+        let otroPrograma = prompt("Quieres otro software?")
+        if (otroPrograma == "si"){
+            let programaExtra = prompt ("Ingresa el nombre del programa deseado");
+            buscarPrograma(programaExtra);
+        }
+        else {
+            alert("Ya tienes lista tu configuración");
+        }
+
+        }
+        else{
+            alert("Programa no disponible o no deseas ningún sofware");
+        }
+    
+
+    }
+    else{
+        alert ("Entonces el precio de tu producto no se verá alterado.");
+    }
+   
+    //Buscaremos dentro del array de objetos el producto por su id para ponerlo dentro de la config del pc.
+    function buscarPrograma(respuesta){
+        const softwareFiltro = softwares.find((producto) => producto.id == respuesta);
+        console.log(softwareFiltro);
+        alert("Se te sumara un precio de" + " "+ softwareFiltro.valor + "$" + " "+ "al total");
+    }
+
+    //Mostraremos la configuracion del pc con su hardware propio, definidos en el objeto.
     function mostrarDatosPc(tier,proces,grafico,rams,discoduro){
 
         alert("Para este presupuesto te vamos a recomendar un pc de tipo"+ " " + tier +" "+ "y va a contener los siguientes componentes:" +  "\n" +" " + proces + " " + "\n" + " "  + grafico + " " + "\n" + " "  + rams + " " + "\n" + " "  + discoduro);
