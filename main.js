c67bd09a7f8c3c33868873c7f6ae50def97ffe88
fcorcoll <francesc901@gmail.com>
2022-01-26 22:01:59 +0100

    DOmCorrecto

--- main.js
+++ main.js
@@ -17,33 +17,33 @@ const resultado = document.querySelector("#resultado");
 botonClick.addEventListener("click",()=>{
 
     
-    
     if (input.value < 300){
-        resultado.textContent `Configuracion no disponible`
+    
         console.log("Configuración no disponible");
-        resultado.textContent = `hola`
+        resultado.textContent = `Lo sentimos pero no es posible jugar con una tostadora`
     }
     else if (input.value <= 500 && input.value >= 300){
         const configuracion1 = new ConfiguracionGaming("Tipo Tier5","Procesador Intel Core i5-10400/i3", "Memória 8gbRam DDr4", "Gráfica HDintel Integrada ","Disco Sólido 1TB HDD");
         console.log(configuracion1.tipo);
         //console.log(configuracion1.equipacion);
         resultado.textContent=mostrarDatosPc(configuracion1.tipo,configuracion1.procesador,configuracion1.grafica,configuracion1.ram,configuracion1.memoria);
-        resultado.textContent = `hola`;
+       
     }
     else if (input.value <= 800 && input.value > 500){
         const configuracion2 = new ConfiguracionGaming("Tipo Tier4","Procesador Intel Core i5-10400","Gráfica GTX1650/Gtx1050ti"," Memória 16gbRam 2600Mhz"," Disco duro 1TBHDD / 512SSD");
         console.log(configuracion2.tipo);
        // console.log(configuracion2.equipacion);
        resultado.textContent=mostrarDatosPc(configuracion2.tipo,configuracion2.procesador,configuracion2.grafica,configuracion2.ram,configuracion2.memoria);
-       resultado.textContent = `hola`;
+    
     }
     else if (input.value <= 1000 && input.value > 800){
         
         const configuracion3 = new ConfiguracionGaming("Tipo Tier3","Procesador AMD Ryzen5/ o Intel core i5", "Gráfica Gtx 1650/Gtx2050ti/1650Super","Memória 16gbRam 3200Mhz","Disco duro 1tbHDD +512SSd");
         console.log(configuracion3.tipo);
         //console.log(configuracion3.equipacion);
-        //resultado.textContent= mostrarDatosPc(configuracion3.tipo,configuracion3.procesador,configuracion3.grafica,configuracion3.ram,configuracion3.memoria);
-        resultado.textContent = "Tipo Tier3: Procesador AMD Ryzen5/ o Intel core i5, Gráfica Gtx 1650/Gtx2050ti/1650Super , Memória 16gbRam 3200Mhz, Disco duro 1tbHDD +512SSd" ;
+        resultado.textContent= mostrarDatosPc(configuracion3.tipo,configuracion3.procesador,configuracion3.grafica,configuracion3.ram,configuracion3.memoria);
+       // resultado.textContent = "Tipo Tier3: Procesador AMD Ryzen5/ o Intel core i5, Gráfica Gtx 1650/Gtx2050ti/1650Super , Memória 16gbRam 3200Mhz, Disco duro 1tbHDD +512SSd" ;
+       
     }
     else if (input.value <= 1200 && input.value > 1000){
      
@@ -51,7 +51,7 @@ botonClick.addEventListener("click",()=>{
         console.log(configuracion4.tipo);
         //console.log(configuracion4.equipacion);
         resultado.textContent=mostrarDatosPc(configuracion4.tipo,configuracion4.procesador,configuracion4.grafica,configuracion4.ram,configuracion4.memoria);
-        resultado.textContent = `hola`;
+        
         }
     else if (input.value > 1200){
         
@@ -59,13 +59,14 @@ botonClick.addEventListener("click",()=>{
         //este console log lo uso para ver que me está entrando bien dentro del objeto, el usuario solo verá los datos por alert que se usan en la funcion.
         console.log(configuracion5.tipo);
        //console.log(configuracion5.equipacion);
-       //resultado.textContent=mostrarDatosPc(configuracion5.tipo,configuracion5.procesador,configuracion5.grafica,configuracion5.ram,configuracion5.memoria);
-       resultado.textContent = `hola`;
+       resultado.textContent=mostrarDatosPc(configuracion5.tipo,configuracion5.procesador,configuracion5.grafica,configuracion5.ram,configuracion5.memoria);
+       
        
     }
 
     else{
         console.warn("no se trata de un dato válido, ingrese otro valor.")
+        resultado.textContent  = `El dato no es valido`
 
     }
 });
@@ -124,9 +125,10 @@ const softwares = [{id :"windows10", valor: 50},
 
     //Mostraremos la configuracion del pc con su hardware propio, definidos en el objeto.
     function mostrarDatosPc(tier,proces,grafico,rams,discoduro){
-
-        "Para este presupuesto te vamos a recomendar un pc de tipo"+ " " + tier +" "+ "y va a contener los siguientes componentes:" +  "\n" +" " + proces + " " + "\n" + " "  + grafico + " " + "\n" + " "  + rams + " " + "\n" + " "  + discoduro;
-        return;
+        let pc;
+        pc = "Para este presupuesto te vamos a recomendar un pc de tipo"+ " " + tier +" "+ "y va a contener los siguientes componentes:" +  "\n" +" " + proces + " " + "\n" + " "  + grafico + " " + "\n" + " "  + rams + " " + "\n" + " "  + discoduro;
+        return pc;
+        
         }
    
    
